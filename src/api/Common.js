030763ae35700bc51b85f2ecc2edb317b880b580
vivek-vimal <vimalvivek50@gmail.com>
2024-03-04 09:44:06 +0530

    ..

--- src/api/Common.js
+++ src/api/Common.js
@@ -2,9 +2,7 @@ import axios from "axios";
 import { BASE_URL } from "../constants";
 
 const token = window.localStorage.getItem("token");
-console.log(`token`, token);
 const formatToken = `Bearer ${token}`;
-console.log(`formatToken`, formatToken);
 
 export const commonAxios = axios.create({
   baseURL: BASE_URL,
